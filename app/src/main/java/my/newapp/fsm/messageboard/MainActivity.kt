78cd11a8534d77b9f828ef162e2eb1e4b26375b4
fsm13 <fomichevsm@gmail.com>
2023-04-15 12:03:24 +0700

    Creating Navigation View

--- app/src/main/java/my/newapp/fsm/messageboard/MainActivity.kt
+++ app/src/main/java/my/newapp/fsm/messageboard/MainActivity.kt
@@ -2,10 +2,62 @@ package my.newapp.fsm.messageboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import my.newapp.fsm.messageboard.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+        binding.navView.setNavigationItemSelectedListener(this)
+
+    }
+    private fun init() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            findViewById(R.id.toolbar),
+            R.string.open,
+            R.string.close
+            )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.id_my_ads -> {
+
+            }
+            R.id.id_car -> {
+
+            }
+            R.id.id_pc -> {
+
+            }
+            R.id.id_smartphone -> {
+
+            }
+            R.id.id_dm -> {
+
+            }
+            R.id.id_sign_up -> {
+
+            }
+            R.id.id_sign_in -> {
+
+            }
+            R.id.id_sign_out -> {
+
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
